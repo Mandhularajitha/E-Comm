f034f44d46dbdc511a3f7039a8d46446f8721057
Rajitha <91309820+Mandhularajitha@users.noreply.github.com>
2022-04-15 16:39:19 +0530

    Update products.js

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -1,5 +1,8 @@
 import { v4 as uuid } from "uuid";
 
+import {ring1, ring2, ring3,ring4,ring5,ring6,ring7,pro1,pro2,pro3,pro4,pro5,pro6,card1,card2,card3} from  "../../assets/images/index" 
+
+
 /**
  * Product Database can be added here.
  * You can add products of your wish with different attributes
@@ -8,28 +11,10 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
+    img:pro1,
+    name:"Jewellary",
     price: "5000",
-<<<<<<< Updated upstream
-    categoryName: "non-fiction",
-  },
-  {
-    _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
-  },
-  {
-    _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
-=======
     rating: 4.5,
-    discountedPrice:999,
     isaddToCart:"true",
     iswishList:"false",
     category:"Sliver"
@@ -41,7 +26,6 @@ export const products = [
     name:"Jewellary",
     price: "2999",
     rating:3,
-    discountedPrice:1999,
     isaddToCart:"true",
     iswishList:"false",
     category:"Cluster"
@@ -52,18 +36,16 @@ export const products = [
     name:"Jewellary",
     price: "5040",
     rating:2,
-    discountedPrice:1000,
     isaddToCart:"true",
     iswishList:"false",
     category:"Glory Mesh"
   },
   {
     _id: uuid(),
-    img:pro1,
+    img:pro4,
     name:"Jewellary",
     price: "5999",
     rating:3.5,
-    discountedPrice:999,
     isaddToCart:"true",
     iswishList:"false",
     category:"Sliver"
@@ -74,7 +56,6 @@ export const products = [
     name:"Jewellary",
     price: "1990",
     rating:4,
-    discountedPrice:990,
     isaddToCart:"true",
     iswishList:"false",
     category:"Cluster"
@@ -85,10 +66,9 @@ export const products = [
     name:"Jewellary",
     price: "3099",
     rating:2,
-    discountedPrice:1959,
     isaddToCart:"true",
     iswishList:"false",
     category:"Cluster"
->>>>>>> Stashed changes
   },
+  
 ];
