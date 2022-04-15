097773b064f9525a5553a3cb8dd09ac7320d323a
Rajitha <91309820+Mandhularajitha@users.noreply.github.com>
2022-04-15 16:13:54 +0530

    Update cartContext.js

--- src/context/cartContext/cartContext.js
+++ src/context/cartContext/cartContext.js
@@ -1,4 +1,5 @@
 
+
 import axios from 'axios';
 import React from 'react'
 import {useState} from 'react'
@@ -28,7 +29,6 @@ console.log(cartData)
                             }
                         })
                     setcartData(res.data.cart)
-                    console.log(res,"kalavathi")
 
                 } catch (error) {
                     console.error(error , "err in getcartData")
@@ -54,8 +54,43 @@ async function addToCart (product) {
     }
 }
 
+async function removeFromCart (id) {
+    try {
+        const res = await axios.delete(`/api/user/cart/${id}` , {
+            headers: {
+                authorization : authtoken
+            }})
+            setcartData(res.data.cart)
+    } catch (error) {
+        console.error(error , "err in increaseQuantity-context");
+    }
+}    
+async function getQuantity (id, type) {
+    try {
+        const res = await axios.post(`/api/user/cart/${id}` , {
+            action: {
+              type: type
+            }
+          }, {
+            headers: {
+                authorization : authtoken
+            }})
+            setcartData(res.data.cart)
+    } catch (error) {
+        console.error(error , "err in removefromcart-context");
+    }
+}
+    
+const priceDetailsCalc = (obj , curritem) => ({ ...obj,
+
+    totalPrice: curritem.price*curritem.qty + obj.totalPrice ,
+    totalDiscoutPrice : curritem.discountedPrice*curritem.qty + obj.totalDiscoutPrice ,
+  })
+
+const reducedData = cartData.reduce(priceDetailsCalc ,{ totalPrice : 0 ,totalDiscoutPrice : 0 })
+
     return (
-      <cartContext.Provider value={{cartData,addToCart}}>
+      <cartContext.Provider value={{cartData,addToCart,removeFromCart,reducedData,getQuantity}}>
           {children}
       </cartContext.Provider>
   )
